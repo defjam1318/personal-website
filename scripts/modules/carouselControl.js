d639a473084b587ddcb3716649d2c032a36025a9
tomailiev <iliev.toma@gmail.com>
2020-09-15 00:27:42 -0400

    added some styles

--- scripts/modules/carouselControl.js
+++ scripts/modules/carouselControl.js
@@ -3,21 +3,28 @@ export function carouselControl(indexCount) {
         e.preventDefault();
     });
     $('#image-0').removeClass('hidden');
+    $('#caption-0').removeClass('hidden');
     let currentIndex = 0;
     $('.carousel-control-prev').click((e) => {
         $(`#image-${currentIndex}`).addClass('hidden');
+        $(`#caption-${currentIndex}`).addClass('hidden');
+
         currentIndex--;
         if (currentIndex < 0) {
             currentIndex = indexCount;
         }
         $(`#image-${currentIndex}`).removeClass('hidden');
+        $(`#caption-${currentIndex}`).removeClass('hidden');
+
     });
     $('.carousel-control-next').click((e) => {
         $(`#image-${currentIndex}`).addClass('hidden');
+        $(`#caption-${currentIndex}`).addClass('hidden');
         currentIndex++;
         if (currentIndex > indexCount) {
             currentIndex = 0;
         }
         $(`#image-${currentIndex}`).removeClass('hidden');
+        $(`#caption-${currentIndex}`).removeClass('hidden');
     });
 }
